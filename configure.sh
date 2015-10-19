1de7f7ed9b497862d7b2bb4add3521de261f026a
Zach Pomerantz <zmp@umich.edu>
2015-10-19 15:57:39 -0700

    (git) alias.hist

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 # git
 git config --global user.email "zmp@umich.edu"
 git config --global user.name "Zach Pomerantz"
+git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
 
 # vim
 wget -O ~/.vimrc https://raw.githubusercontent.com/zzmp/setup/master/.vimrc
